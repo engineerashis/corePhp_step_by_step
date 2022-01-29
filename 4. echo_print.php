5b6e454d232fde899d90b53ab47792c84c812d3b
developerashisroy <ashisroy40089@gmail.com>
2022-01-30 02:19:12 +0530

    different between echo and print

---
+++ 4. echo_print.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>echo and print statement</title>
+</head>
+<body>
+    <p>always use echo statement . basic defferent echo and print is when we ues , in two statement ehco show output  but print don't show output </p>
+    <?php
+      $first_name="Ashis ";
+      $last_name=" Roy";
+      $full_name="$first_name" . "$last_name";
+     echo $full_name;
+    //  print $full_name;
+    //echo has no return value but print has 1 return value
+    // multiple parameters in echo , single argumrnt in print
+    //echo is faster then print
+    
+    
+    
+    
+    
+    ?>
+</body>
+</html>
