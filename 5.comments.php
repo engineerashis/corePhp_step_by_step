5d1741cdf7a4f81b1c226252ec24b8ad213f518a
developerashisroy <ashisroy40089@gmail.com>
2022-01-30 03:11:50 +0530

    comment last

---
+++ 5.comments.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>comment statements</title>
+</head>
+<body>
+    <?php
+     //two type of comment
+     //single line comment start with slash//
+     // or start with hashF#
+     //multiline comment start with /*   and  end with */
+     $fisrt_name="Ashis ";// single line comment;
+     $last_name="Roy";        #another type of single comment;
+     echo $fisrt_name;
+     echo $last_name;
+     /**
+      * 
+
+      this is multiline comment and avoid it
+      */
+
+
+
+    ?>
+</body>
+</html>
