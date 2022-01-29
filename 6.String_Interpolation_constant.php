6179ec255b9c542e0c07853ce1ec78789140a2db
developerashisroy <ashisroy40089@gmail.com>
2022-01-30 03:53:41 +0530

    interpolararion

---
+++ 6.String_Interpolation_constant.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>string interpolaration in php</title>
+</head>
+<body>
+       <?php
+         $name="india ";
+         echo "welcome to : $name <br>";    //right
+        // echo "$namehello";// wrong
+        //define(constanat variable, value, case-sensitive)
+        define("num", 48 , true);
+        echo num;
+
+       ?>
+    
+</body>
+</html>
