506318089e52d1b7bf62c3a24797300fa059632b
developerashisroy <ashisroy40089@gmail.com>
2022-01-30 01:28:26 +0530

    first core php code

---
+++ 1.Basic_structure_of_php.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introduction to basic php </title>
+</head>
+<style>
+  .container{
+    text-align:center;
+    border:2px solid green;
+  }
+  h1,h4{
+    background: yellow;
+    color:#000;
+  }
+</style>
+<body>
+     <div class="container">
+        <h1>Development Environment to write and run php codes</h1>
+        <h2>1.Operating system - windows, linux , Mac</h2>
+        <h3>2.Web server  XAMM, WAMP, MAMP etc </h3>
+        <p>2.1 XAMM- Apache+MarisDB+PHP+perl</p>
+        <h3>3.Editors/IDE- NotePad, NotePad++, NetBeans ,Brackets, Eclipse, Vs code , sublime text editors etc</h3>
+        <p>3.1 I use this serice in VS Code editor.</p>
+        <h3>4.Web browser- chrome, Firefox, opera, saferi etc</h3>
+        <P>4.1 but i use chrome and Firefox</P>
+        <h4>all php code are run in the local host</h4>
+     </div>
+   <!-- php code start-->
+    
+   
+
+   <?php // Opennig tag or markup;
+
+      echo "hello india";  // this is statement; 
+
+    ?> 
+    <!--  Closing markup -->
+
+     <!-- php code start-->
+</body>
+</html>
