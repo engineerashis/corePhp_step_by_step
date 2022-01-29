5061e813182ba4ad9b99073c3167a4a33faf0a99
developerashisroy <ashisroy40089@gmail.com>
2022-01-30 01:56:04 +0530

    data type of php

---
+++ 3.DataType.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>data type of php</title>
+</head>
+<body>
+    <p>Actually not define php data types, its  automatically  handle </p>
+    <?php
+      // integer
+          //$price_data=3112;
+          // $price_value=3112.96;
+      //string
+          // $first_name="Sintheya Roy";
+      //Boolean
+          // True/ Falase... 0 and 1;
+      //Array
+          // $cars_name=array("volve","tata", "Shymoli","BMW");
+      //Object
+           // $display=new ObjectName();
+      //Null 
+    
+    ?>
+</body>
+</html>
