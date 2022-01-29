bfcc05335f5f40acd5c41cd27aa2b2e8ca2dcb8a
developerashisroy <ashisroy40089@gmail.com>
2022-01-30 01:45:26 +0530

    php variable

---
+++ 2.Variable.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variable in php</title>
+
+</head>
+<body>
+    <p>variable in php</p>
+    <?php
+     // there are three type of variable in php
+     // 1.local variable
+     // 2.Global variable 
+     // 3. static variable
+     // variable name define $ sign 
+     // some variable names $roll, $price, $first_name etc
+     // variable not start with a number
+     
+     $roll;// declaration
+     $roll=30; //initialization
+     echo $roll;//print
+     
+     
+     echo "<br>";
+     $first_name="Ashis ";
+     $last_name="Roy";
+     echo $first_name;
+     echo $last_name;
+
+
+
+
+    ?>
+</body>
+</html>
