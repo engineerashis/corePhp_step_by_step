2294f7e782127b25817ec6b3dfdc3858b47e2c52
developerashisroy <ashisroy40089@gmail.com>
2022-01-30 12:59:19 +0530

    ternary operators

---
+++ 18.Ternary.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ternary operators</title>
+</head>
+<body>
+    <?php 
+    $per=40;
+    $result=($per>50)?"greater than : $per" : "less than: $per";
+    echo $result;
+    
+    
+    
+    
+    ?>
+</body>
+</html>
