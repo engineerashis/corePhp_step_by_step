2989b4f41633968a24cc25c204459adf4c88a613
developerashisroy <ashisroy40089@gmail.com>
2022-01-30 11:28:11 +0530

    nested if else

---
+++ 16.nestedifelse.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>nested if else statement</title>
+</head>
+<body>
+    <?php
+       if(4>2){
+           if(6<5){
+               echo " false statement <br>";
+           }else{
+               echo "true statement<br>";
+           }
+       }else{
+           echo "other statement <br>";
+       }
+       echo "rest of the statement";
+    ?>
+</body>
+</html>
