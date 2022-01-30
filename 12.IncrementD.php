beb748e00299804a88f8bfd78524e8027939e594
developerashisroy <ashisroy40089@gmail.com>
2022-01-30 10:38:01 +0530

    increment operators

---
+++ 12.IncrementD.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>increment and decrement operators</title>
+</head>
+<body>
+    <?php
+       $x=4;
+       $y=4;
+       echo "<br>".$x;
+       echo "<br>".$x++;
+       echo "<br>".$x;
+       echo "<br>".$y;
+       echo "<br>".++$y;
+       echo "<br>".$y;
+
+       echo "<br>".$x;
+       echo "<br>".$x--;
+       echo "<br>".$x;
+       echo "<br>".$y;
+       echo "<br>".--$y;
+       echo "<br>".$y;
+    ?>
+</body>
+</html>
