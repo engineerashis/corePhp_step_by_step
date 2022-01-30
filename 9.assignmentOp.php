264e5993524ca6e8689c5b331f4f10c874d934c6
developerashisroy <ashisroy40089@gmail.com>
2022-01-30 09:56:50 +0530

    assignment operators

---
+++ 9.assignmentOp.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assignment operators in php</title>
+</head>
+<body>
+    <?php
+     // assignment operator in php = , += , -=, *=, /=, %= 
+     $a=10;
+     $a++;
+     echo "$a . <br>";
+
+     $a=10;
+     $a+=20;;
+     echo "$a . <br>";
+
+     $a=10;
+     $a-=20;;
+     echo "$a . <br>";
+
+     $a=10;
+     $a*=20;;
+     echo "$a . <br>";
+
+     $a=10;
+     $a/=20;;
+     echo "$a . <br>";
+
+     $a=10;
+     $a%=20;;
+     echo "$a . <br>";
+
+
+    ?>
+</body>
+</html>
